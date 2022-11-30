fa0f026627b1046c9db588038eb5ee456cd159e4
Tinsae <iamtinsae.dev@gmail.com>
2022-11-30 17:43:23 +0300

    Update

--- gen.py
+++ gen.py
@@ -2,12 +2,16 @@ inp1 = input("First Number: ")
 inp2 = input("Second Number: ")
 inp3 = input("Third Number: ")
 
+
 def sum_number_parts(s):
     while len(s) != 1:
         s = str(sum(map(int, list(s))))
 
-    return s
-
-print (int(sum_number_parts(inp1)) + int(sum_number_parts(inp2)) + int(sum_number_parts(inp3)))
+    return int(s)
 
 
+s1 = sum_number_parts(inp1)
+s2 = sum_number_parts(inp2)
+s3 = sum_number_parts(inp3)
+
+print (sum_number_parts(str(s1 + s2 + s3)))
